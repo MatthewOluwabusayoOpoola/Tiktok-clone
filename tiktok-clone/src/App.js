43c905cd095a8c86a4c051efbfd6e619ec506e45
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-16 19:11:19 +0100

    First commit
    frontend for the tiktok clone

---
+++ tiktok-clone/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__videos">
+        <Video url='"./short.mp4"'
+        channel={"Testing 123"}
+        description={"Testing 123"}
+        song={"Testing 123"}
+        likes={"Testing 123"}
+        messages={"Testing 123"}
+        shares={"Testing 123"} />
+      </div>
+      
+      {/* videos */}
+      {/*  */}
+    </div>
+  );
+}
+
+export default App;
